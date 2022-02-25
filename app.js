713eb75037326e280707af84d3d5c55826d15855
hitpop2216 <hitpop2216@gmail.com>
2022-02-25 15:59:48 +0800

    Add: static pages

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const exphbs = require('express-handlebars')
 const app = express()
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
+app.use(express.static('public'))
 
 app.get('/', (req, res) => {
   res.render('index')
