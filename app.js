8d835ac3123cea1be8146c933574c0a2a67ec15c
hitpop2216 <hitpop2216@gmail.com>
2022-02-25 16:14:39 +0800

    Revise: dynamic pages

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
+const restaurantList = require('./restaurant.json').results
 
 const app = express()
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
@@ -7,7 +8,12 @@ app.set('view engine', 'hbs')
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-  res.render('index')
+  res.render('index', {restaurantList})
+})
+app.get('/restaurants/:id', (req, res) => {
+  const id = req.params.id.toString()
+  const restaurant = restaurantList.find(item => item.id.toString() === id)
+  res.render('show', { restaurant })
 })
 
 app.listen(3000, () =>{
