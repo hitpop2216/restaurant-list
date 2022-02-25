83f4468b562de82a47200adeeb4f7ff6bad03937
hitpop2216 <hitpop2216@gmail.com>
2022-02-25 16:38:54 +0800

    Add: models

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
+const mongoose = require('mongoose')
 const restaurantList = require('./restaurant.json').results
+
+mongoose.connect('mongodb://localhost/restaurant-list')
+const db = mongoose.connection
+db.on('error', () => console.log('mongodb error!'))
+db.once('open', () => console.log('mongodb connected!'))
 
 const app = express()
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
@@ -14,6 +20,12 @@ app.get('/restaurants/:id', (req, res) => {
   const id = req.params.id.toString()
   const restaurant = restaurantList.find(item => item.id.toString() === id)
   res.render('show', { restaurant })
+})
+
+app.get('/search', (req, res) => {
+  const keyword = req.query.keyword.trim().toLowerCase()
+  const restaurants = restaurantList.filter(item => item.name.trim().toLowerCase().includes(keyword) || item.category.trim().toLowerCase().includes(keyword))
+  res.render('index', { restaurantList: restaurants})
 })
 
 app.listen(3000, () =>{
