941afd40fd062af73e0dab4f459f05eb808939a9
hitpop2216 <hitpop2216@gmail.com>
2022-02-26 16:23:29 +0800

    Add: sorting function

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -14,8 +14,31 @@ router.get('/', (req, res) => {
 // 搜尋
 router.get('/search', (req, res) => {
   const keyword = req.query.keyword.trim().toLowerCase()
-  const restaurants = restaurantList.filter(item => item.name.trim().toLowerCase().includes(keyword) || item.category.trim().toLowerCase().includes(keyword))
-  res.render('index', { restaurantList: restaurants })
+  const sort = req.query.sort
+  let sortingMethod = {}
+  switch (sort) {
+    case 'A->Z':
+      sortingMethod = { name: 'asc' }
+      break;
+    case 'Z->A':
+      sortingMethod = { name: 'desc' }
+      break;
+    case '類別':
+      sortingMethod = { category: 'asc' }
+      break;
+    case '地區':
+      sortingMethod = { location: 'asc' }
+      break;
+  }
+  Restaurant
+    .find()
+    .lean()
+    .sort(sortingMethod)
+    .then(items => {
+      const restaurants = items.filter(item => item.name.trim().toLowerCase().includes(keyword) || item.category.trim().toLowerCase().includes(keyword))
+      res.render('index', { restaurantList: restaurants})
+    })
+    .catch(err => console.log(err))
 })
 
 module.exports = router
